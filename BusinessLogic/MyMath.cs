8a4f2a86a8b6fa2788a2c7498dd7855060481cde
Vo Quang Hoa <voquanghoa@gmail.com>
2020-07-05 22:05:42 +0700

    Add code

---
+++ BusinessLogic/MyMath.cs
@@ -0,0 +1,22 @@
+namespace BusinessLogic
+{
+    public class MyMath
+    {
+        public int Gcd(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ BusinessLogicTest/MyMathTest.cs
@@ -0,0 +1,17 @@
+using BusinessLogic;
+using NUnit.Framework;
+
+namespace BusinessLogicTest
+{
+    public class MyMathTest
+    {
+        [Test]
+        [TestCase(9, 18, 27)]
+        [TestCase(10, 30, 40)]
+        public void TestGcd(int result, int a, int b)
+        {
+            var myMath = new MyMath();
+            Assert.AreEqual(result, myMath.Gcd(a, b));
+        }
+    }
+}
